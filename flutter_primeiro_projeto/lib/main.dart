1996b8b3ffa7b66c3ac7ad5db618cee6540c8e83
João Pedro <jpedrodaibello1999@outlook.com>
2021-12-05 22:04:02 -0300

    feat: add Device Preview package

--- flutter_primeiro_projeto/lib/main.dart
+++ flutter_primeiro_projeto/lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_primeiro_projeto/pages/home/container/container_page.dart';
+import 'package:flutter_primeiro_projeto/pages/container/container_page.dart';
 import 'package:flutter_primeiro_projeto/pages/home/home_page.dart';
 import 'package:flutter_primeiro_projeto/pages/media_query/media_query_page.dart';
 import 'package:flutter_primeiro_projeto/pages/rows_columns/rows_columns_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (_) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,6 +25,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       routes: {
         '/': (_) => const HomePage(),
         '/container': (_) => const ContainerPage(),
