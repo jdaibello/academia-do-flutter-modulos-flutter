3346ea44f102e7a8aebad88269ce58bef42bf480
João Pedro <jpedrodaibello1999@outlook.com>
2021-12-11 18:43:06 -0300

    feat: add understanding how to update and roll back database version

--- flutter_sqlite_example/lib/database/database_sqlite.dart
+++ flutter_sqlite_example/lib/database/database_sqlite.dart
@@ -9,28 +9,79 @@ class DatabaseSqLite {
 
     await openDatabase(
       databaseFinalPath,
-      version: 1,
+      version: 2,
+      onConfigure: (db) async {
+        debugPrint('onConfigure chamado');
+
+        await db.execute('PRAGMA foreign_keys = ON');
+      },
 
       // Chamado somente no momento de criação do banco de dados
       // primeira vez que carrega o aplicativo
       onCreate: (Database db, int version) {
         debugPrint('onCreate chamado');
-
         final batch = db.batch();
+
         batch.execute('''
-          CREATE TABLE teste(
+          CREATE TABLE produto(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nome VARCHAR(200)
           )
         ''');
+
+        // batch.execute('''
+        //   CREATE TABLE categoria(
+        //     id INTEGER PRIMARY KEY AUTOINCREMENT,
+        //     nome VARCHAR(200)
+        //   )
+        // ''');
         batch.commit();
       },
 
       // Será chamado sempre que houver alteração no version incremental (1 -> 2)
-      onUpgrade: (Database db, int oldVersion, int newVersion) {},
+      onUpgrade: (Database db, int oldVersion, int newVersion) {
+        debugPrint('onUpgrade chamado');
+        final batch = db.batch();
+
+        if (oldVersion == 1) {
+          batch.execute('''
+            CREATE TABLE produto(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              nome VARCHAR(200)
+            )
+        ''');
+
+          //   batch.execute('''
+          //     CREATE TABLE categoria(
+          //       id INTEGER PRIMARY KEY AUTOINCREMENT,
+          //       nome VARCHAR(200)
+          //     )
+          // ''');
+        }
+
+        // if (oldVersion == 2) {
+        //   batch.execute('''
+        //     CREATE TABLE categoria(
+        //       id INTEGER PRIMARY KEY AUTOINCREMENT,
+        //       nome VARCHAR(200)
+        //     )
+        // ''');
+        // }
+        batch.commit();
+      },
 
       // Será chamado sempre que houver alteração no version decremental (2 -> ')
-      onDowngrade: (Database db, int oldVersion, int newVersion) {},
+      onDowngrade: (Database db, int oldVersion, int newVersion) {
+        debugPrint('onDowngrade chamado');
+        final batch = db.batch();
+
+        if (oldVersion == 3) {
+          batch.execute('''
+            DROP TABLE categoria
+        ''');
+        }
+        batch.commit();
+      },
     );
   }
 }
