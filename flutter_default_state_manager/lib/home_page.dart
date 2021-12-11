949d050bea72363247380809e35070ecf269d1ff
João Pedro <jpedrodaibello1999@outlook.com>
2021-12-10 21:14:35 -0300

    feat: add creating project and building layout

---
+++ flutter_default_state_manager/lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_default_state_manager/setState/imc_set_state_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  void _goToPage(BuildContext context, Widget page) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => page),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => _goToPage(context, const ImcSetStatePage()),
+              child: const Text('setState'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('ValueNotifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('ChangeNotifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Bloc Pattern (Streams)'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
