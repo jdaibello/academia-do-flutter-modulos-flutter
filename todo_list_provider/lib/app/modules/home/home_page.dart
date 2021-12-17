f129c867e96ef9fa2b1c56261baf3a01a2b90aa7
João Pedro <jpedrodaibello1999@outlook.com>
2021-12-16 21:57:32 -0300

    feat: add changing user name

--- todo_list_provider/lib/app/modules/home/home_page.dart
+++ todo_list_provider/lib/app/modules/home/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list_provider/app/core/auth/auth_provider.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,14 +10,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            context.read<AuthProvider>().logout();
-          },
-          child: const Text('Logout'),
-        ),
-      ),
+      drawer: HomeDrawer(),
+      body: Container(),
     );
   }
 }
---
+++ todo_list_provider/lib/app/modules/home/widgets/home_drawer.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_overlay_loader/flutter_overlay_loader.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/core/auth/auth_provider.dart';
+import 'package:todo_list_provider/app/core/ui/messages.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/services/user/user_service.dart';
+
+class HomeDrawer extends StatelessWidget {
+  final nameVN = ValueNotifier<String>('');
+
+  HomeDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: context.primaryColor.withAlpha(70),
+            ),
+            child: Row(
+              children: [
+                Selector<AuthProvider, String>(
+                  selector: (context, authProvider) {
+                    return authProvider.user?.photoURL ??
+                        'https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg';
+                  },
+                  builder: (_, value, __) {
+                    return CircleAvatar(
+                      backgroundImage: NetworkImage(value),
+                      radius: 30,
+                    );
+                  },
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Selector<AuthProvider, String>(
+                      selector: (context, authProvider) {
+                        return authProvider.user?.displayName ??
+                            'Não informado';
+                      },
+                      builder: (_, value, __) {
+                        return Text(
+                          value,
+                          style: context.textTheme.subtitle2,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return AlertDialog(
+                    title: const Text('Alterar Nome'),
+                    content: TextField(
+                      onChanged: (value) => nameVN.value = value,
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text(
+                          'Cancelar',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          final nameValue = nameVN.value;
+
+                          if (nameValue.isEmpty) {
+                            Messages.of(context).showError('Nome obrigatório');
+                          } else {
+                            Loader.show(context);
+                            await context
+                                .read<UserService>()
+                                .updateDisplayName(nameValue);
+                            Loader.hide();
+                            Navigator.of(context).pop();
+                          }
+                        },
+                        child: const Text('Alterar'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            title: const Text('Alterar Nome'),
+          ),
+          ListTile(
+            onTap: () => context.read<AuthProvider>().logout(),
+            title: const Text('Sair'),
+          ),
+        ],
+      ),
+    );
+  }
+}
